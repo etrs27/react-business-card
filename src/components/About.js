18eaaba4cfc7137a3f85881f90a59424bb09e61f
Theodore Stansbury <tstansbu.27@gmail.com>
2022-05-11 15:15:54 -0400

    Edited styling for About component.

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,7 @@
-import profilePhoto from '../images/profile-photo.jpeg'
 import './About.css'
+import profilePhoto from '../images/profile-photo.jpeg'
+import emailIcon from '../images/email-icon.png'
+import linkedinIcon from '../images/linkedin-icon.png'
 
 export default function About() {
     return (
@@ -9,8 +11,18 @@ export default function About() {
             <p className="about--position">Software Engineer</p>
             <a className="about--website" href="https://github.com/etrs27">Website</a>
             <div className="about--buttons">
-                <a className="about--email" href="#">Email</a>
-                <a className="about--linkedin" href="#">LinkedIn</a>
+                <div className="about--email">
+                    <a className="about--links" href="#">
+                        <img src={emailIcon} alt="email icon" className="about--icons"/>
+                    </a>
+                    <p className="about--texts">Email</p>
+                </div>
+                <div className="about--linkedin">
+                    <a className="about--links" href="#">
+                        <img src={linkedinIcon} alt="linkedin icon" className="about--icons"/>
+                    </a>
+                    <p className="about--texts">LinkedIn</p>
+                </div>
             </div>
         </header>
     )
